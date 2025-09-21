1467326409666974279e40fb050cacea8ebf6717
Akhilesh Malthi <akhileshmalthi2299@gmail.com>
2025-09-21 14:02:07 +0530

    Remove extraction templates, task classes, configuration manager, and related files to streamline the project structure and eliminate unused components.

--- agents/__init__.py
+++
@@ -1,9 +0,0 @@
-from .filtering_agent import FilteringAgent
-from .task_extraction_agent import TaskExtractionAgent
-from .task_formatting_agent import TaskFormattingAgent
-
-__all__ = [
-    'FilteringAgent',
-    'TaskExtractionAgent',
-    'TaskFormattingAgent'
-]
